00a43cc1fbb6408f668d7fa60606f583174146b4
Alena Chao <alenachao2@gmail.com>
2024-11-28 00:08:24 -0800

    added combined model tab

--- demo-app/app.py
+++ demo-app/app.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 import urllib.parse
 
 # load models
-mlm_model_path = "/Users/harsita/Desktop/btt/btt-google-2e/demo-app/mlm_model_save"
-gpt_model_path = "/Users/harsita/Desktop/btt/btt-google-2e/demo-app/gpt_model_save2"
+mlm_model_path = "./maskedLM"
+gpt_model_path = "./fine_tuned_model"
 
 mask_filler = pipeline('fill-mask', model=mlm_model_path, tokenizer=mlm_model_path, framework='tf')
 gpt_model = TFGPT2LMHeadModel.from_pretrained(gpt_model_path)
@@ -90,7 +90,7 @@ st.markdown(
 )
 
 # create tabs for the two functionalities
-tabs = st.tabs(["MaskedLM", "GPT2"])
+tabs = st.tabs(["MaskedLM", "GPT2", "Combined"])
 
 with tabs[0]:
     # display informational message to user
@@ -107,7 +107,7 @@ with tabs[0]:
         else:
             # get top 5 predictions from maskedlm model
             results = mask_filler(query, top_k=5)
-
+            
             # display the results 
             st.markdown("<div class='result'>", unsafe_allow_html=True)
             st.markdown("<p><b>Top Predictions:</b></p>", unsafe_allow_html=True)
@@ -132,10 +132,10 @@ with tabs[0]:
 
 with tabs[1]:
     # message for user
-    st.info("Enter a prompt to get predictions from our GPT model.")
+    st.info("Enter an incomplete prompt to get predictions from our GPT model.")
 
     # input field for user 
-    prompt = st.text_input(label="Enter your prompt:", placeholder="e.g., Interesting facts about Egypt")
+    prompt = st.text_input(label="Enter your prompt:", placeholder="e.g., Interesting facts about")
 
     # button for text generation
     if st.button("Generate", key="gpt_button"):
@@ -145,22 +145,21 @@ with tabs[1]:
         else:
             # tokenize and truncate the input prompt
             input_ids_full = gpt_tokenizer(prompt, return_tensors='tf')
-            n = 3  # number of tokens to truncate for input
-            input_ids = tf.expand_dims(input_ids_full['input_ids'][0][:-n], 0)
-            attention_mask = input_ids_full['attention_mask'][:, :-n]
+            input_ids = input_ids_full['input_ids']
+            attention_mask = input_ids_full['attention_mask']
 
             # generate predictions with controlled parameters
             outputs = gpt_model.generate(
                 input_ids=input_ids,
                 attention_mask=attention_mask,
                 pad_token_id=gpt_tokenizer.eos_token_id,
-                max_length=input_ids.shape[1] + n + 10,
+                max_length=input_ids.shape[1] + 10,
                 num_return_sequences=5,  # generate top 5 sequences
                 no_repeat_ngram_size=2,
                 do_sample=True,
-                top_k=17,
+                top_k=15,
                 top_p=0.5,
-                temperature=0.8,
+                temperature=0.5,
                 repetition_penalty=2.0
             )
 
@@ -190,3 +189,73 @@ with tabs[1]:
                 )
 
             st.markdown("</div>", unsafe_allow_html=True)
+
+    with tabs[2]:
+        # message for user
+        st.info("Enter an incomplete prompt to get predictions from our combined model.")
+
+        # input field for user 
+        prompt = st.text_input(label="Enter your prompt:", placeholder="What is the oldest")
+
+        # button for text generation
+        if st.button("Generate", key="combined_button"):
+            # check if the prompt is empty
+            if not prompt:
+                st.error("Please enter a prompt.")
+            else:
+                # tokenize and truncate the input prompt
+                input_ids_full = gpt_tokenizer(prompt, return_tensors='tf')
+                input_ids = input_ids_full['input_ids']
+                attention_mask = input_ids_full['attention_mask']
+
+                # generate predictions with controlled parameters
+                outputs = gpt_model.generate(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,
+                    pad_token_id=gpt_tokenizer.eos_token_id,
+                    max_length=input_ids.shape[1] + 10,
+                    num_return_sequences=3,  # generate top 5 sequences
+                    no_repeat_ngram_size=2,
+                    do_sample=True,
+                    top_k=15,
+                    top_p=0.5,
+                    temperature=0.5,
+                    repetition_penalty=2.0
+                )
+
+                # decode and post-process the outputs
+                predictions = []
+                for output in outputs:
+                    gpt_prediction = gpt_tokenizer.decode(output, skip_special_tokens=True)
+
+                    # add mask token at the end and feed into mlm to get final prediction
+                    masked_query = gpt_prediction + " <mask> ?"
+                    mlm_predictions = mask_filler(masked_query, top_k = 2)
+                    for pred in mlm_predictions:
+                        predictions.append(pred['sequence'])
+
+                # display results
+                st.markdown("<div class='result'>", unsafe_allow_html=True)
+                st.markdown("<p><b>Generated Texts:</b></p>", unsafe_allow_html=True)
+
+                # loop through predictions and create clickable links for each prediction
+                for prediction in predictions:
+                    if not prediction.endswith("?"):
+                        prediction += "?"
+                    encoded_query = urllib.parse.quote(prediction)
+                    search_url = f"https://www.google.com/search?q={encoded_query}"
+
+                    st.markdown(
+                        f"""
+                        <a href="{search_url}" target="_blank">
+                            <div class="prediction-box">
+                                {prediction}
+                            </div>
+                        </a>
+                        """,
+                        unsafe_allow_html=True
+                    )
+
+                st.markdown("</div>", unsafe_allow_html=True)
+
+
